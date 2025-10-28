22bd55744a63db9188560bd7c0a52a05fb2547ad
Violet <3551664030@qq.com>
2025-10-28 13:29:30 +0800

    Initial

--- test.py
+++ test.py
@@ -3,22 +3,21 @@ import glob
 import torch
 import gc
 
-model = YOLO('runs/detect/model_based_s2/weights/best.pt')
+model = YOLO('runs/detect/model_based_s/weights/best.pt')
 
 test_images = glob.glob('E:/Dataset/Fruits 2.v1-three.yolov11/test/images/*.jpg')
 
 results = model.predict(
     source=test_images,
     imgsz=320,      # 输入图像尺寸
-    conf=0.40,      # 置信度阈值
+    conf=0.25,      # 置信度阈值
     iou=0.45,       # IoU阈值
     device=0,
     save=True,      # 保存预测结果
     save_txt=True,  # 保存标签文件
     save_conf=True, # 保存置信度
     save_crop=True,  # 保存裁剪的检测目标
-    batch=4,
-    name='based_s2_predictions',  # 结果保存目录名称
+    name='based_s_predictions',  # 结果保存目录名称
 )
 
 del results  # 删除引用
--- train.py
+++ train.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # Load a model
     # model = YOLO("yolo11n.yaml")  # build a new model from YAML
-    model = YOLO('runs/detect/model_based_s2/weights/last.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('runs/detect/model_based_s/weights/last.pt')  # load a pretrained model (recommended for training)
     # model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
     # Train the model
-    # results = model.train(data='E:\Dataset\Fruit.v1i.yolov11/data.yaml', epochs=50, imgsz=640, batch=16, device=0, name='model_based_s')
-    model.train(resume=True,batch=16)
+    results = model.train(data='E:\Dataset\Fruit.v1i.yolov11/data.yaml', epochs=100, imgsz=640, batch=16, device=0, name='model_based_s2')
+    # model.train(resume=True,batch=16)
